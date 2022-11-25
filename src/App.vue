9187598b1310e812cdc6f474f3bb627cf441f054
richardjim <richardiyevhobu@gmail.com>
2022-11-25 13:51:23 +0100

    set up pinia ad getters

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
-<script setup>
-
-</script>
 
 <template>
- 
+  <main>
+    <!--heading-->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="logo" />
+      <h1>Pinia Task</h1>
+    </header>
+    <!--filter-->
+    <nav class="filter">
+      <button @click="filter = 'all'">All Task</button>
+      <button @click="filter = 'favs'">All Favorite</button>
+    </nav>
+    <!--task list-->
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{taskStore.totalCount }} tasks left to do</p>
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{taskStore.favCount }} tasks left to do</p>
+      <div v-for="task in taskStore.favs">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped>
+<script>
+import { useTaskStore } from './stores/TaskStore';
+import TaskDetails from './components/TaskDetails.vue'
+import { ref } from 'vue';
+export default {
+  components: {
+    TaskDetails
+  },
+  setup() {
+    const taskStore = useTaskStore();
+    const filter = ref('all')
 
-</style>
+    return { taskStore, filter }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-
+import './assets/main.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+
+const pinia = createPinia()
+
+
+createApp(App).use(pinia).mount('#app')
+
